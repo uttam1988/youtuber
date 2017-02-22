8513794a7b6f7adb3f1814e0d167b0b9e748ff51
uttam <uttam12061988@gmail.com>
2017-02-22 11:01:22 +0530

    My Code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// import App from './components/app';
+// import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
+/*ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container'));*/
+
+
+const App = () => {
+    return <div>Hi there I am a component</div>;
+}
+
+ReactDOM.render(<App/>,document.querySelector('.container'))
